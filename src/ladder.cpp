b792bbef73cd1608959baa5b39bb89b05370921c
T-LXY <tonyrms2018@gmail.com>
2025-03-17 03:32:49 +0000

    Minor bug fix.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -6,6 +6,28 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
+    // int len1 = str1.size();
+    // int len2 = str2.size();
+
+    // if (abs(len1 - len2) > d) return false;
+
+    // vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+
+    // for (int i = 0; i <= len1; ++i) 
+    //     dp[i][0] = i;
+
+    // for (int j = 0; j <= len2; ++j) 
+    //     dp[0][j] = j;
+
+    // for (int i = 1; i <= len1; ++i) {
+    //     for (int j = 1; j <= len2; ++j) {
+    //         if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+    //         else dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+    //     }
+    // }
+
+    // return dp[len1][len2] <= d;
+
     int len1 = str1.size();
     int len2 = str2.size();
     int difference = 0;
