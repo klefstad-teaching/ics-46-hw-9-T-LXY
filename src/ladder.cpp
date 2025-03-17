0768b6b35e1b02288fbb1b0cb0d706e81fd92c94
T-LXY <tonyrms2018@gmail.com>
2025-03-17 02:35:04 +0000

    Changed edit_distance_within.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -6,42 +6,61 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
+    // int len1 = str1.size();
+    // int len2 = str2.size();
+
+    // if (abs(len1 - len2) > d) return false;
+
+    // vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+
+    // for (int i = 0; i <= len1; ++i) 
+    //     dp[i][0] = i;
+
+    // for (int j = 0; j <= len2; ++j) 
+    //     dp[0][j] = j;
+
+    // for (int i = 1; i <= len1; ++i) {
+    //     for (int j = 1; j <= len2; ++j) {
+    //         if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+    //         else dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+    //     }
+    // }
+
+    // return dp[len1][len2] <= d;
+
     int len1 = str1.size();
     int len2 = str2.size();
+    int difference = 0;
 
     if (abs(len1 - len2) > d) return false;
 
-    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
-
-    for (int i = 0; i <= len1; ++i) 
-        dp[i][0] = i;
-
-    for (int j = 0; j <= len2; ++j) 
-        dp[0][j] = j;
-
-    for (int i = 1; i <= len1; ++i) {
-        for (int j = 1; j <= len2; ++j) {
-            if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
-            else dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+    for (int i = 0, j = 0; difference <= d and i < len1 and j < len2; ++i, ++j){
+        if (str1[i] != str2[j]){
+            if (len1 < len2) --i;
+            else if (len1 > len2) --j;
+            ++difference;
         }
+
+        if (i == len1) difference += len2 - j;
+        else if (j == len2) difference += len1 - i;
     }
 
-    return dp[len1][len2] <= d;
+    return difference <= d;
 }
 
 bool is_adjacent(const string& word1, const string& word2){ 
     return edit_distance_within(word1, word2, 1);
 }
 
-// string lower(const string& str){
-//     string lower(str);
-//     transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
-//     return lower;
-// }
+string lower(const string& str){
+    string lower(str);
+    transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
+    return lower;
+}
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
-    // string begin = lower(begin_word);
-    // string end = lower(end_word);
+    string begin = lower(begin_word);
+    string end = lower(end_word);
 
     if (begin_word == end_word or !word_list.contains(end_word)) return {};
 
@@ -56,12 +75,13 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         string last_word = ladder.back();
 
         for (const string& word : word_list){
-            if (is_adjacent(last_word, word) && !visited.contains(word)){
-                visited.insert(word);
+            string lword = lower(word);
+            if (is_adjacent(last_word, lword) && !visited.contains(lword)){
+                visited.insert(lword);
                 vector<string> new_ladder = ladder;
-                new_ladder.push_back(word);
+                new_ladder.push_back(lword);
 
-                if (word == end_word) return new_ladder;
+                if (lword == end_word) return new_ladder;
                 ladder_queue.push(new_ladder);
             }
         }
